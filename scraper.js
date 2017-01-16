59c5c6e81798bc01ab947423b0d0c2d7a1aeb455
Steven Nagie <sanagie@yahoo.com>
2017-01-16 16:42:09 -0700

    Installed fb package, which is hopefully the right track.

---
+++ scraper.js
@@ -0,0 +1,18 @@
+var config = require('./config.json');
+
+window.fbAsyncInit = function() {
+    FB.init({
+      appId      : config.appId,
+      xfbml      : true,
+      version    : 'v2.8'
+    });
+    FB.AppEvents.logPageView();
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
