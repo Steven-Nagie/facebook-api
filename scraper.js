ef0a8a8cf931f79ca823e0a740ce7c74ceb5f788
Steven Nagie <sanagie@yahoo.com>
2017-01-17 12:24:20 -0700

    fb node module correctly grabbing an access token from the graph api.

--- scraper.js
+++ scraper.js
@@ -1,18 +1,30 @@
+var FB = require('fb');
 var config = require('./config.json');
 
-window.fbAsyncInit = function() {
-    FB.init({
-      appId      : config.appId,
-      xfbml      : true,
-      version    : 'v2.8'
-    });
-    FB.AppEvents.logPageView();
-  };
+var accessToken;
+FB.options({version: 'v2.8'});
+// var app = FB.extend({appId: config.appId, appSecret: config.appSecret});
+//
+// app.setAccessToken();
+//
+// app.api('me', function (res) {
+//   if(!res || res.error) {
+//    console.log(!res ? 'error occurred' : res.error);
+//    return;
+//   }
+//   console.log(res.id);
+//   console.log(res.name);
+// });
 
-  (function(d, s, id){
-     var js, fjs = d.getElementsByTagName(s)[0];
-     if (d.getElementById(id)) {return;}
-     js = d.createElement(s); js.id = id;
-     js.src = "//connect.facebook.net/en_US/sdk.js";
-     fjs.parentNode.insertBefore(js, fjs);
-   }(document, 'script', 'facebook-jssdk'));
+// To generate an App access token
+FB.api(`oauth/access_token?client_id=${config.appId}&client_secret=${config.appSecret}&grant_type=client_credentials`, function(res) {
+
+  if (!res || res.error) {
+    console.log(!res ? 'error occurred' : res.error);
+    return;
+  }
+
+  accessToken = res.access_token;
+  console.log('access token: ', accessToken);
+
+})
