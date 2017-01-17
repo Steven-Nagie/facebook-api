cd0ecc4424ef765022574aac0b41263a40413b35
Steven Nagie <sanagie@yahoo.com>
2017-01-17 12:51:33 -0700

    Propeperly getting access token, setting access token, and hitting appId enpoint.

--- scraper.js
+++ scraper.js
@@ -3,18 +3,19 @@ var config = require('./config.json');
 
 var accessToken;
 FB.options({version: 'v2.8'});
-// var app = FB.extend({appId: config.appId, appSecret: config.appSecret});
-//
-// app.setAccessToken();
-//
-// app.api('me', function (res) {
-//   if(!res || res.error) {
-//    console.log(!res ? 'error occurred' : res.error);
-//    return;
-//   }
-//   console.log(res.id);
-//   console.log(res.name);
-// });
+var app = FB.extend({appId: config.appId, appSecret: config.appSecret});
+
+// response for appId endpoint contains category, link, name, and id
+getMe = () => {
+  app.api(`${config.appId}`, function (res) {
+    if(!res || res.error) {
+     console.log(!res ? 'error occurred' : res.error);
+     return;
+    }
+    console.log(res);
+
+  });
+}
 
 // To generate an App access token
 FB.api(`oauth/access_token?client_id=${config.appId}&client_secret=${config.appSecret}&grant_type=client_credentials`, function(res) {
@@ -25,6 +26,7 @@ FB.api(`oauth/access_token?client_id=${config.appId}&client_secret=${config.appS
   }
 
   accessToken = res.access_token;
-  console.log('access token: ', accessToken);
-
+  console.log("access token: ", accessToken);
+  app.setAccessToken(accessToken);
+  getMe();
 })
