b48908c448f08621a6e73d938786d5d6b25ce67a
Steven Nagie <sanagie@yahoo.com>
2017-01-18 12:58:02 -0700

    Now getting page info, album info, photo info, and photo likes.

--- scraper.js
+++ scraper.js
@@ -5,8 +5,8 @@ var accessToken;
 FB.options({version: 'v2.8'});
 var app = FB.extend({appId: config.appId, appSecret: config.appSecret});
 
-// response for appId endpoint contains category, link, name, and id
-getMe = () => {
+// Default response for appId endpoint contains category, link, name, and id
+getApp = () => {
   app.api(`${config.appId}`, function (res) {
     if(!res || res.error) {
      console.log(!res ? 'error occurred' : res.error);
@@ -14,6 +14,54 @@ getMe = () => {
     }
     console.log(res);
 
+  });
+}
+
+// The 'fields' query paramters determine what the response contains.
+getAppEmail = () => {
+  app.api(`${config.appId}?fields=contact_email`, function (res) {
+    if(!res || res.error) {
+     console.log(!res ? 'error occurred' : res.error);
+     return;
+    }
+    console.log(res);
+
+  });
+}
+
+getPagePhotoLikes = (photoId) => {
+  app.api(`${photoId}/likes`, function(res) {
+    if(!res || res.error) {
+     console.log(!res ? 'error occurred' : res.error);
+     return;
+    }
+    console.log(res);
+    var totalLikes = res.data.length;
+    console.log("total likes: ", totalLikes);
+  });
+}
+
+// Grabbing a page's photos, using an album id that we get from getPage();
+getPagePhotos = (albumId) => {
+  app.api(`${albumId}/photos`, function(res) {
+    if(!res || res.error) {
+     console.log(!res ? 'error occurred' : res.error);
+     return;
+    }
+    // console.log(res);
+    getPagePhotoLikes(res.data[0].id)
+  });
+}
+
+// Grabbing a page based on id, then grabbing their albums
+getPage = () => {
+  app.api(`19292868552/albums`, function(res) {
+    if(!res || res.error) {
+     console.log(!res ? 'error occurred' : res.error);
+     return;
+    }
+    // console.log(res);
+    getPagePhotos(res.data[1].id)
   });
 }
 
@@ -28,5 +76,7 @@ FB.api(`oauth/access_token?client_id=${config.appId}&client_secret=${config.appS
   accessToken = res.access_token;
   console.log("access token: ", accessToken);
   app.setAccessToken(accessToken);
-  getMe();
+  // getApp();
+  // getAppEmail();
+  getPage();
 })
